a208dddf5428b734af607caf844f164f21dcd087
pbussi@gmail.com <pbussi@gmail.com>
2018-08-22 09:18:09 -0300

    agregado de metodos admin. config en archivo separado

--- routes/multichain.js
+++ routes/multichain.js
@@ -2,21 +2,17 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 
-var chain = 'devnet';
+/*var chain = 'devnet';
 var burnaddress = '1XXXXXXXd4XXXXXXgMXXXXXXUPXXXXXXTvtX2d';
 var masteraddress = '17jRtkVEGJmo9KkK15E9tyviquVtuWt8t8VnMJ';
-var asset = "Pesos";
-var options = {
-  url: "http://158.69.63.214:8080/",
-  method: "POST",
-  headers: {
-    "content-type": "text/plain"
-  },
-  auth: {
-    user: 'multichainrpc',
-    pass: 'Gwc9pNzRvAv4dyQB6QW8LzbVxuMBsYWXoUytXBJyS3UT'
-  },
-};
+var asset = "Pesos";*/
+var config = require('../config.json');
+var chain = config.chain;
+var options = config.options;
+var burnaddress = config.burnaddress;
+var masteraddress = config.masteraddress;
+var asset = config.asset;
+
 
 /*******************************************************************/
 /*******************************************************************/
@@ -347,6 +343,61 @@ router.get('/movimientos/:address/:codigo_m', function(req, res, next) {
 });
 
 
+/*******************************************************************/
+/*******************************************************************/
+/*******************************************************************/
+router.get('/admin/:clave/cambia_codigo/:cartera_id/:nuevo_codigo_m', function(req, res, next) {
+  if (req.params.clave != options.admin_clave) {
+    res.send({
+      "error": "codigo incorrecto"
+    });
+  } else {
+    options.body = JSON.stringify({
+      "method": "publishfrom",
+      "params": [req.params.cartera_id, "codigos", req.params.nuevo_codigo_m, "3230323335353032393835"],
+      "id": 1,
+      "chain_name": chain
+    });
+
+    request(options, (error, response, body) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.json(JSON.parse(body));
+      }
+    });
+  }
+});
+
+/*******************************************************************/
+/*******************************************************************/
+/*******************************************************************/
+router.get('/admin/:clave/movimientos/:address', function(req, res, next) {
+  if (req.params.clave != config.admin_clave) {
+    res.send({
+      "error": "codigo incorrecto"
+    });
+  } else {
+  options.body = JSON.stringify({
+    "method": "listaddresstransactions",
+    "params": [req.params.address],
+    "id": 1,
+    "chain_name": chain
+  })
+  request(options, (error, response, body) => {
+    if (error) {
+      res.send(error);
+    } else {
+      var todos = JSON.parse(body);
+      todos = todos.result;
+      var movimientos = todos.filter(function(d) {
+        return d.balance.assets.length > 0;
+      });
+      res.json(movimientos);
+    }
+  });
+}
+});
 
 
 module.exports = router;
